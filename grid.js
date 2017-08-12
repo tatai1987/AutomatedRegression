8fdb45baa140f5623e4293c85ba37d513045360a
tatai1987 <subhadeep.bose@in.ibm.com>
2017-08-12 20:03:43 +0530

    test

--- grid.js
+++ grid.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     //Initialize tooltips
     $('.nav-tabs > li a[title]').tooltip();
-    
+
     //Wizard
     $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
 
         var $target = $(e.target);
-    
+
         if ($target.parent().hasClass('disabled')) {
             return false;
         }
@@ -35,71 +35,78 @@ function prevTab(elem) {
 }
 
 function updateMDF() {
-       
-        var excel = new ActiveXObject("Excel.Application");
-        
+    var excel = new ActiveXObject("Excel.Application");
+    if (test_type == 'regression') {
         var excel_file = excel.Workbooks.Open("C:\\Automation\\regression\\MDF.xlsx");
-       
-        
-        var excel_sheet = excel_file.Worksheets("Applications");
-        
-        var data = excel_sheet.Cells(2,2).Value;
-       
+    }
+    else {
+        var excel_file = excel.Workbooks.Open("C:\\Automation\\smoke\\MDF.xlsx");
+    }
 
-        var rowSize= excel_sheet.Rows.Count;
-         for(var i=1;i<=rowSize;i++){
-                excel_sheet.Cells(i,4).Value ='No';
-            }
-        
+    var excel_sheet = excel_file.Worksheets("Applications");
 
-        for(var i=1;i<=rowSize;i++){
-            var data =excel_sheet.Cells(i,1).Value;
-            if(data =='PTE' && excel_sheet.Cells(i,2).Value =='NWPFE_AUTH'){
-                excel_sheet.Cells(i,4).Value ='Yes';
-                console.info(excel_sheet.Cells(i,4).Value);
-            }
+    var rowSize = excel_sheet.Rows.Count;
+    for (var i = 1; i <= rowSize; i++) {
+        excel_sheet.Cells(i, 4).Value = 'No';
+    }
+
+    for (var i = 1; i <= rowSize; i++) {
+        if (excel_sheet.Cells(i, 1).Value == 'PTE' && excel_sheet.Cells(i, 2).Value == 'NWPFE_AUTH') {
+            excel_sheet.Cells(i, 4).Value = 'Yes';
         }
-         
-         excel.Workbooks.Saved = true;
-         excel.Workbooks.Close=true;
-         excel.Workbooks.Application.Quit();
-       
+    }
+
+    var excel_sheet = excel_file.Worksheets("Global");
+    var rowSize = excel_sheet.Rows.Count;
+    for (var i = 1; i <= rowSize; i++) {
+        excel_sheet.Cells(i, 7).Value = 'N';
+    }
+
+    for (var i = 1; i <= rowSize; i++) {
+        if (excel_sheet.Cells(i, 8).Value == 'NWPFE_AUTH') {
+            excel_sheet.Cells(i, 7).Value = 'Y';
+        }
+    }
+
+    excel.Workbooks.Saved = true;
+    excel.Workbooks.Close = true;
+    excel.Workbooks.Application.Quit();
+
 }
 
 function runVBS() {
-        alert('Inside Run VBS');
-        var shell = new ActiveXObject("WScript.Shell");
-        shell.Exec("wscript C:\\Automation\\Execute.vbs"); 
+    alert('Inside Run VBS');
+    var shell = new ActiveXObject("WScript.Shell");
+    shell.Exec("wscript C:\\Automation\\Execute.vbs");
 }
 
 //according menu
 
-$(document).ready(function()
-{
+$(document).ready(function () {
     //Add Inactive Class To All Accordion Headers
-	$('.accordion-header').toggleClass('inactive-header');
-	
-	//Set The Accordion Content Width
-	var contentwidth = $('.accordion-header').width();
-	$('.accordion-content').css({});
-	
-	//Open The First Accordion Section When Page Loads
-	$('.accordion-header').first().toggleClass('active-header').toggleClass('inactive-header');
-	$('.accordion-content').first().slideDown().toggleClass('open-content');
-	
-	// The Accordion Effect
-	$('.accordion-header').click(function () {
-		if($(this).is('.inactive-header')) {
-			$('.active-header').toggleClass('active-header').toggleClass('inactive-header').next().slideToggle().toggleClass('open-content');
-			$(this).toggleClass('active-header').toggleClass('inactive-header');
-			$(this).next().slideToggle().toggleClass('open-content');
-		}
-		
-		else {
-			$(this).toggleClass('active-header').toggleClass('inactive-header');
-			$(this).next().slideToggle().toggleClass('open-content');
-		}
-	});
-	
-	return false;
+    $('.accordion-header').toggleClass('inactive-header');
+
+    //Set The Accordion Content Width
+    var contentwidth = $('.accordion-header').width();
+    $('.accordion-content').css({});
+
+    //Open The First Accordion Section When Page Loads
+    $('.accordion-header').first().toggleClass('active-header').toggleClass('inactive-header');
+    $('.accordion-content').first().slideDown().toggleClass('open-content');
+
+    // The Accordion Effect
+    $('.accordion-header').click(function () {
+        if ($(this).is('.inactive-header')) {
+            $('.active-header').toggleClass('active-header').toggleClass('inactive-header').next().slideToggle().toggleClass('open-content');
+            $(this).toggleClass('active-header').toggleClass('inactive-header');
+            $(this).next().slideToggle().toggleClass('open-content');
+        }
+
+        else {
+            $(this).toggleClass('active-header').toggleClass('inactive-header');
+            $(this).next().slideToggle().toggleClass('open-content');
+        }
+    });
+
+    return false;
 });
