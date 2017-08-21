31de645250eb6da72e052b386c0e0cc6f53a2564
pragatimrinal <prmrinal@in.ibm.com>
2017-08-21 15:33:18 +0530

    modified for mongo db connection

--- automatedRegression.go
+++ automatedRegression.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"net/http"
-	"os"
-	"strings"
+	"time"
+
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var target string
@@ -13,8 +15,22 @@ const (
 	DEFAULT_PORT = "8080"
 )
 
+//const MongoDb details
+const (
+	hosts    = "127.0.0.1:27017"
+	database = "ikeasocialapp"
+	//username   = "admin"
+	//password   = "youPassword"
+	//collection = "messages"
+)
+
+type Person struct {
+	Name  string
+	Phone string
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	/*http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
 		var urlvalue = r.URL.Path[1:]
 		if strings.Compare(urlvalue, "confirmation.html") == 0 {
@@ -33,5 +49,39 @@ func main() {
 	if port = os.Getenv("PORT"); len(port) == 0 {
 		port = DEFAULT_PORT
 	}
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))*/
+
+	info := &mgo.DialInfo{
+		Addrs:    []string{hosts},
+		Timeout:  60 * time.Second,
+		Database: database,
+		//Username: username,
+		//Password: password,
+	}
+
+	session, err := mgo.DialWithInfo(info)
+	if err != nil {
+		panic(err)
+	}
+
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB(database).C("people")
+	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
+		&Person{"Cla", "+55 53 8402 8510"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := Person{}
+	err = c.Find(bson.M{"name": "Ale"}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Phone:", result.Phone)
+
 }
