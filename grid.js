0c4d0fe4a1eb8b25234938e2732d1064fa7f4059
Agnibina Sengupta <agngupta@in.ibm.com>
2017-08-22 14:23:26 +0530

    Changes for the heading

--- grid.js
+++ grid.js
@@ -125,7 +125,7 @@ function readMRF() {
     }
 $('#dyn-table').html('<table class="table"><tbody></tbody></table>');
 
-for (var i = 1; i < countrow; i++) {
+for (var i = 0; i < countrow; i++) {
     
     $('#dyn-table tbody').append( '<tr><td>' + titleList[i]
                         + '</td> <td>'+ testCaseIdList[i]
